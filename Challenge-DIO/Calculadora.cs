797115a3aa19ae205e84ac35dc8f9ba55c64e1f3
Rafael Kozlowski Andreola <rafandreola@hotmail.com>
2023-09-14 10:09:31 -0300

    Add files via upload

---
+++ Challenge-DIO/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge_DIO
+{
+    public class Calculadora
+    {
+        private List<string> listaHistorico;
+        private string data;
+
+        public Calculadora(string data)
+        {
+            listaHistorico = new List<string>();
+            this.data = data;
+        }
+
+        public int somar(int val1, int val2)
+        {
+            int resultado = val1 + val2;
+
+            listaHistorico.Insert(0, "Resultado: " + resultado + " - data: " + data);
+            return resultado;
+        }
+
+        public int subtrair(int val1, int val2)
+        {
+            int resultado = val1 - val2;
+
+            listaHistorico.Insert(0, "Resultado: " + resultado + " - data: " + data);
+            return resultado;
+        }
+
+        public int multiplicar(int val1, int val2)
+        {
+            int resultado = val1 * val2;
+
+            listaHistorico.Insert(0, "Resultado: " + resultado + " - data: " + data);
+            return resultado;
+        }
+
+        public int dividir(int val1, int val2)
+        {
+            int resultado = val1 / val2;
+
+            listaHistorico.Insert(0, "Resultado: " + resultado + " - data: " + data);
+            return resultado;
+        }
+
+        public List<string> historico()
+        {
+            listaHistorico.RemoveRange(3, listaHistorico.Count-3);
+            return listaHistorico;
+        }
+    }
+}
---
+++ Challenge-DIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Challenge_DIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
